0a1bdc45708a717f8909152194643020c0930834
delfiardius <ardigulo09@gmail.com>
2025-04-04 20:12:52 +0700

    modified a tipe of suppported variabel from list to set

--- src/main/kotlin/koleksi/practice/ProtocolServer.kt
+++ src/main/kotlin/koleksi/practice/ProtocolServer.kt
@@ -1,7 +1,7 @@
 package com.dicodingdelfi.kotlin.koleksi.practice
 
 fun main() {
-    val SUPPORTED = listOf("HTTP", "HTTPS", "FTP")
+    val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
     val requested = "smtp"
     val isSupported = requested.uppercase() in SUPPORTED // cek the request in a list, if yes, the result request is true, is no then false
     println("Support for $requested: $isSupported") // requested false
